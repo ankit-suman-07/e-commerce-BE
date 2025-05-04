b14a4e1bccee8f9efb7e92927a73934e79c06ce2
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-05-05 00:24:51 +0530

    Create test api

---
+++ src/main/java/com/assign/e_commerce/adapter/in/web/ProductController.java
@@ -0,0 +1,16 @@
+package com.assign.e_commerce.adapter.in.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class ProductController {
+
+    @GetMapping("/test")
+    public String testProject() {
+        return "Project is Running!!!";
+    }
+}
